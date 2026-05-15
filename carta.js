035e965fcd6b24461c647477f5f20a08641cbcb9
Lautaro Valentino Guzzio <lauguzzio@gmail.com>
2026-05-15 19:29:55 -0300

    implement card class

---
+++ carta.js
@@ -0,0 +1,39 @@
+class Carta {
+    constructor(code, value, suit, image) {
+        this.code = code;
+        this.value = value;
+        this.suit = suit;
+        this.image = image;
+    }
+
+    toJsonString() {
+        return JSON.stringify(this);
+    }
+
+    static createFromJsonString(json) {
+        const data = JSON.parse(json);
+        return new Carta(data.code, data.value, data.suit, data.image);
+    }
+
+    createHtmlElement() {
+        const container = document.createElement('div');
+        container.className = 'carta-container';
+
+        const imgElement = document.createElement('img');
+        imgElement.src = this.image;
+        imgElement.className = 'carta-img';
+
+        const infoElement = document.createElement('div');
+        infoElement.className = 'carta-info';
+        infoElement.innerHTML = `
+            <p><strong>Codigo:</strong> ${this.code}</p>
+            <p><strong>Palo:</strong> ${this.suit}</p>
+            <p><strong>Valor:</strong> ${this.value}</p>
+        `;
+
+        container.appendChild(imgElement);
+        container.appendChild(infoElement);
+
+        return container;
+    }
+}
